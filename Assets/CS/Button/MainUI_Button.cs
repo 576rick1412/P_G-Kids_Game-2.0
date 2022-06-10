b4ef93f8f5c78927a381972ba57761d339e362f8
576rick1412 <576rick1412@gmail.com>
2022-06-11 03:40:02 +0900

    이미지 불러오기

---
+++ Assets/CS/Button/MainUI_Button.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUI_Button : MonoBehaviour
+{
+    public void Hub_Scene()
+    {
+        SceneManager.LoadScene("Main_Scene");
+    }
+    public void PuzzleGame_Scene()
+    {
+        SceneManager.LoadScene("PuzzleGame_Hub");
+    }
+
+    public void MedicGame_Scene()
+    {
+        SceneManager.LoadScene("MedicGame_Hub");
+    }
+
+    public void AniamlGame_Scene()
+    {
+        SceneManager.LoadScene("AnimalGame_Hub");
+    }
+
+    public void AppleGame_Scene()
+    {
+        SceneManager.LoadScene("AppleGame_Hub");
+    }
+
+    public void Main_Scene()
+    {
+        SceneManager.LoadScene("Title_Scene");
+    }
+    public void goHome()
+    {
+        SceneManager.LoadScene("Title_Scene");
+    }
+    public void proto_b()
+    {
+        Debug.Log("세팅 눌림");
+    }
+    public void goQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void MedicQues_1()
+    {
+        Debug.Log("물음표11");
+    }
+}
