6879b7d659b3e6ac9e2417359b05bf1c88e9a6e9
576rick1412 <576rick1412@gmail.com>
2022-06-16 03:45:34 +0900

    4스테이지 동물게임 제작완료
    토끼 -> 끼토 등의 자유로운 글자 배열 + 글자가 겹치지 않도록 추가함

--- Assets/CS/미니게임/4_동물게임/Animal_Manager.cs
+++ Assets/CS/미니게임/4_동물게임/Animal_Manager.cs
@@ -12,7 +12,9 @@ public class Animal_Manager : MonoBehaviour
     [Header("단어 순서")]
     public int[] OBJ_order; // 오브젝트 순서
     public bool[] cheak; // 드롭 확인용
-    
+    public bool[] Null_cheak; // 드롭 확인용
+    public int[] Null_cheak_Num; //드롭 확인용222
+
     public GameObject[] GameObject; //글자박스
     public GameObject[] target; // 빈칸
     Vector2[] target_POS = new Vector2[5];
@@ -20,47 +22,24 @@ public class Animal_Manager : MonoBehaviour
     void Start()
     {
         Null_Num = Screen.height / 5.6f; //화면 비율이 비례해서 범위가 바뀌기 위해
-
-        for (int i = 0; i < OBJ_Num; i++)
-        {
-            target_POS[i] = GameObject[i].transform.position;
-        }
-
-        for (int i = 0; i < cheak.Length; i++)
-        {
-            cheak[i] = false;
-        }
-
+        for (int i = 0; i < OBJ_Num; i++)       { target_POS[i] = GameObject[i].transform.position; }
+        for (int i = 0; i < cheak.Length; i++)  { cheak[i] = false; }
     }
 
     void Update()
     {
         switch (cheak.Length)
         {
-            case 1:
-                if (cheak[0] == true)
-                { Debug.Log("클리어"); }
-                break;
-            case 2:
-                if (cheak[0] == true && cheak[1] == true)
-                { Debug.Log("클리어"); }
-                break;
-
-            case 3:
-                if (cheak[0] == true && cheak[1] == true && cheak[2] == true)
-                { Debug.Log("클리어"); }
-                break;
-
-            case 4:
-                if (cheak[0] == true && cheak[1] == true && cheak[2] == true && cheak[3] == true)
-                { Debug.Log("클리어"); }
-                break;
+            case 1: if (cheak[0] == true)                                                               { Debug.Log("클리어"); } break;
+            case 2: if (cheak[0] == true && cheak[1] == true)                                           { Debug.Log("클리어"); } break;
+            case 3: if (cheak[0] == true && cheak[1] == true && cheak[2] == true)                       { Debug.Log("클리어"); } break;
+            case 4: if (cheak[0] == true && cheak[1] == true && cheak[2] == true && cheak[3] == true)   { Debug.Log("클리어"); } break;
         }
     }
 
     public void Drag_Animal_0()
     {
-        Drag_Animal(0);
+       Drag_Animal(0);
     }
     public void Drag_Animal_1()
     {
@@ -82,38 +61,55 @@ public class Animal_Manager : MonoBehaviour
     // =====================================================================
     public void Drop_Animal_0()
     {
-        Drop_Animal(0);
+        Null_cheak_Num[0] = Drop_Animal(0);
     }
     public void Drop_Animal_1()
     {
-        Drop_Animal(1);
+        Null_cheak_Num[1] = Drop_Animal(1);
     }
     public void Drop_Animal_2()
     {
-        Drop_Animal(2);
+        Null_cheak_Num[2] = Drop_Animal(2);
     }
     public void Drop_Animal_3()
     {
-        Drop_Animal(3);
+        Null_cheak_Num[3] = Drop_Animal(3);
     }
     public void Drop_Animal_4()
     {
-        Drop_Animal(4);
+        Null_cheak_Num[4] = Drop_Animal(4);
     }
     public void Drag_Animal(int i)
     {
         GameObject[i].transform.position = Input.mousePosition;
         cheak[i] = false;
-    }
-    public void Drop_Animal(int i)
-    {
-        // 글자박스(GameObject)와 빈칸좌표(target)의 ward[n]번쨰와의 거리가 Null_Num보다 작다면 true 멀다면 false
-        float Distance = Vector3.Distance(GameObject[i].transform.position, target[OBJ_order[i]].transform.position);
-        if (Distance < Null_Num)
+        switch (Null_cheak_Num[i])
         {
-            GameObject[i].transform.position = target[OBJ_order[i]].transform.position;
-            cheak[i] = true;    return;
+            case 0: Null_cheak[0] = false; break;
+            case 1: Null_cheak[1] = false; break;
+            case 2: Null_cheak[2] = false; break;
+            case 3: Null_cheak[3] = false; break;
         }
-        GameObject[i].transform.position = target_POS[i];
+    }
+    public int Drop_Animal(int i)
+    {
+        // 글자박스(GameObject)와 빈칸좌표(target)의 OBJ_order[n]번째와의 거리가 Null_Num보다 작다면 true 멀다면 false
+        // 각 글자박스마다 Null_cheak_Num값을 가지며 현재 있는 빈칸의 정보를 담기위해 사용, 만약 빈칸에 다른 글자박스가 있다면 원래위치로 반환, 아니면 빈칸으로 이동
+        // return 0;으로 할 경우 빈칸[0]과 겹치는 경우가 생겨 100으로 반환
+        for (int j = 0; j < target.Length; j++)
+        {
+            float Distance = Vector3.Distance(GameObject[i].transform.position, target[OBJ_order[j]].transform.position);
+            if (Distance < Null_Num)
+            {
+                if (Null_cheak[j] == true) { GameObject[i].transform.position = target_POS[i]; return 100; }
+                else
+                {
+                    if (i == j)
+                    { GameObject[i].transform.position = target[OBJ_order[i]].transform.position;   cheak[i] = true;    Null_cheak[j] = true;   return j; }
+                    else
+                    { GameObject[i].transform.position = target[OBJ_order[j]].transform.position;                       Null_cheak[j] = true;   return j; }
+                }
+            }
+        }GameObject[i].transform.position = target_POS[i]; return 100;
     }
 }
